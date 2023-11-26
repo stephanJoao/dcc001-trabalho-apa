429198c87673e45484282e67164ed319dc6ea9f2
Joao Stephan <joaossmauricio@gmail.com>
2023-11-25 21:03:29 -0300

    algumas alterações

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def shuffle_list():
-	pass
+def create_shuffled_list(size=10, shuffle_rate=0):
+	arr = list(range(size))
+	n_swaps = int(size * shuffle_rate)
+	print(n_swaps)
+	for i in range(n_swaps):
+		a = np.random.randint(0, size)
+		b = np.random.randint(0, size)
+		arr[a], arr[b] = arr[b], arr[a]
+	return arr
 
 def first_pos(arr, lo, hi):
 	return lo
@@ -12,6 +19,16 @@ def middle_pos(arr, lo, hi):
 
 def random_pos(arr, lo, hi):
 	return np.random.randint(lo, hi + 1)
+
+def median(arr, lo, hi):
+	mid = (hi - lo) // 2 + lo 
+	if arr[lo] > arr[mid]:
+		arr[lo], arr[mid] = arr[mid], arr[lo]
+	if arr[mid] > arr[hi]:
+		arr[mid], arr[hi] = arr[hi], arr[mid]
+	if arr[lo] > arr[hi]:
+		arr[lo], arr[hi] = arr[mid], arr[lo]
+	return mid
 
 def partition(arr, lo, hi, find_pivot):
 	# choose pivot
@@ -51,11 +68,19 @@ def altera(arr):
 	arr[0] = 10
 
 if __name__ == "__main__":
-	arr = list(np.random.randint(0, 100, 10))
+	shuffle_rates = [0.05, 0.25, 0.45]
+	pivot_functions = [first_pos, middle_pos, random_pos]
+	n_experiments = 10
+	
+	print(create_shuffled_list(shuffle_rate=0.1))
+	
+	# sizes = [10**i for i in range(1, 6)]
+	# print(sizes)
+	# arr = list(np.random.randint(0, 100000, 100000))
 	# arr = [5, 4, 3, 2, 1]
-	print(arr)
-	quick_sort(arr, 0, len(arr) - 1, random_pos)
-	print(arr)
+	# print(arr)
+	# quick_sort(arr, 0, len(arr) - 1, random_pos)
+	# print(arr)
 	
 	
 
