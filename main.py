2ec7d4549869271cde1d8f24e9a3a561d43d9364
Joao Stephan <joaossmauricio@gmail.com>
2023-12-06 00:19:36 -0300

    novos gráficos, consertar e embelezar um cadin, fazer plot na escala de log a partir do que não tiver 0

--- main.py
+++ main.py
@@ -94,11 +94,12 @@ def quicksort(arr, lo, hi, find_pivot):
 
 # main
 if __name__ == "__main__":
-	sizes = [10**i for i in range(1, 10)]
+	sizes = [10**i for i in range(1, 9)]
 	shuffle_rates = [0.05, 0.25, 0.45]
 	pivot_functions = [middle_pos, median, random_pos, mean, first_pos]
 	n_experiments = 10
-	results = []
+	
+	'''results = []
 	
 	for size in sizes:
 		for shuffle_rate in shuffle_rates:
@@ -112,18 +113,29 @@ if __name__ == "__main__":
 					print([size, shuffle_rate, pivot_function.__name__, i, end - start])
 					results.append([size, shuffle_rate, pivot_function.__name__, i, end - start])
 				results = pd.DataFrame(results, columns=['size', 'shuffle_rate', 'pivot_function', 'experiment', 'time'])
-				name = f'results_{size}_{shuffle_rate}_{pivot_function.__name__}.csv'
-				results.to_csv(name, index=False, sep=';')
+				name = f'results/results_{size}_{shuffle_rate}_{pivot_function.__name__}.csv'
+				results.to_csv(name, index=False, sep=';')''' # experiments
 
-	# print(results)
+	# append all results to one file
+	results = pd.DataFrame()
+	for size in sizes:
+		for shuffle_rate in shuffle_rates:
+			for pivot_function in pivot_functions:
+				name = f'results/results_{size}_{shuffle_rate}_{pivot_function.__name__}.csv'
+				df = pd.read_csv(name, sep=';')
+				results = pd.concat([results, df], ignore_index=True)
+	results.to_csv('results.csv', index=False, sep=';')
 
-	
+
+	# prepare results for plotting
 	df = pd.DataFrame(results)
 	grouped_df = df.groupby(['shuffle_rate', 'pivot_function', 'size'])['time'].mean().reset_index()
 
 	unique_shuffle_rates = grouped_df['shuffle_rate'].unique()
 	unique_pivot_functions = grouped_df['pivot_function'].unique()
 
+	# plot results
+	print('Number of plots: ', len(unique_shuffle_rates) * len(unique_pivot_functions))
 	for pivot_function in unique_pivot_functions:
 		for shuffle_rate in unique_shuffle_rates:
 			subset_df = grouped_df[(grouped_df['pivot_function'] == pivot_function) & (grouped_df['shuffle_rate'] == shuffle_rate)]
@@ -135,7 +147,44 @@ if __name__ == "__main__":
 			plt.title(f'Mean Time for Each Size - Pivot Function: {pivot_function}')
 			plt.legend()
 			
-			filename = f'{shuffle_rate}_{pivot_function}.png'
+			filename = f'plots/normal/{shuffle_rate}_{pivot_function}.png'
 			plt.savefig(filename)
 			
 			plt.close()
+
+	# plot results log
+
+	for pivot_function in unique_pivot_functions:
+		for shuffle_rate in unique_shuffle_rates:
+			subset_df = grouped_df[(grouped_df['pivot_function'] == pivot_function) & (grouped_df['shuffle_rate'] == shuffle_rate)]
+
+			plt.plot(subset_df['size'], subset_df['time'], label=f'Shuffle Rate: {shuffle_rate}, Pivot Function: {pivot_function}', marker='o')
+
+			plt.xlabel('Size')
+			plt.ylabel('Time')
+			plt.xscale('log')
+			plt.yscale('log')
+			plt.title(f'Mean Time for Each Size - Pivot Function: {pivot_function}')
+			plt.legend()
+			
+			filename = f'plots/logscale/{shuffle_rate}_{pivot_function}_log.png'
+			plt.savefig(filename)
+			
+			plt.close()
+
+	# comparison between pivot functions
+
+	for shuffle_rate in unique_shuffle_rates:
+		for pivot_function in unique_pivot_functions:
+			subset_df = grouped_df[(grouped_df['pivot_function'] == pivot_function) & (grouped_df['shuffle_rate'] == shuffle_rate)]
+
+			plt.plot(subset_df['size'], subset_df['time'], label=pivot_function, marker='o')
+			
+			plt.xlabel('Size')
+			plt.ylabel('Time')
+			plt.title(f'Mean Time for Each Size - Size: {size}')
+			plt.legend()			
+			
+		filename = f'plots/comparison/{shuffle_rate}_log.png'
+		plt.savefig(filename)
+		plt.close()
